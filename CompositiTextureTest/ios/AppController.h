a3eaba259fec9d268b8bfb71afaedb71c3a56d0b
fancyzero <fancyzero@live.cn>
2013-02-18 11:02:32 +0800

    Initial Commit

---
+++ CompositiTextureTest/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  CompositiTextureTestAppController.h
+//  CompositiTextureTest
+//
+//  Created by FancyZero on 13-2-18.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ CompositiTextureTest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  CompositiTextureTestAppController.h
+//  CompositiTextureTest
+//
+//  Created by FancyZero on 13-2-18.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
